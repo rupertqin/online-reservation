d42ad7dbaea7894f37e24d870cb26291172f6ab6
rupert <rupert@qianbindeMacBook-Pro.local>
2021-01-10 19:02:34 +0800

    new img

--- src/pages/Intro/Intro.tsx
+++ src/pages/Intro/Intro.tsx
@@ -5,9 +5,9 @@ import './style.scss';
 const wrapperStyle = {
   background: 'url(/img/引导页1/引导页背景.png) top/100% repeat',
 }
-
 const imgStyle = {
   // width: `${703/2}px`,
+  marginBottom: '1rem',
 }
 
 const img2Style = {
@@ -16,14 +16,29 @@ const img2Style = {
 
 const textStyle = {
   color: 'rgb(142	166	167	)',
+  fontSize: '3rem',
+  'text-align': 'center',
+  'font-weight': 'bold',
 }
+
+const boxStyle = {
+  ...textStyle,
+  background: 'url(/img/引导页1/引导页1-文字框.png) top/100% repeat',
+  width: '390px',
+  height: '177px',
+  lineHeight: '177px',
+  display: 'inline-block',
+}
+
 
 export default function Intro() {
   return <div style={wrapperStyle} className="page-intro intro">
     <Link to="/intro/step/1">
       <img style={imgStyle} src="/img/引导页1/引导页1-厨师机器人.png" alt="" />
       <h2 style={textStyle}>Hi,我是XXX</h2>
-      <img style={img2Style} src="/img/引导页1/引导页1-文字框.png" alt="" />
+      <div style={boxStyle}>
+        欢迎来到智慧餐厅
+      </div>
     </Link>
   </div>
 }
--- src/pages/Intro/Step1.tsx
+++ src/pages/Intro/Step1.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import Steps from '@/components/Steps';
 
 const wrapperStyle = {
-  background: 'url(/img/引导页-对话页/引导页2背景.png) no-repeat 100%',
+  background: 'url(/img/引导页2/bg.png) top/100% no-repeat',
 }
 
 
 export default function Intro() {
-  return <div style={wrapperStyle} className="page-intro">
-    <Steps step={1} />
-    <img src="/img/引导页-对话页/引导页-客服机器人.png" alt="" />
+  return <div style={wrapperStyle} className="page-intro steps">
+    <div>
+      <Steps step={1} />
+      <img src="/img/引导页2/引导页-客服机器人.png" alt="" />
+      <img className="dialog" src="/img/引导页客服对话/引导页-对话框性别.png" alt="" />
+    </div>
+    <div>
+      <h2>性别</h2>
+    </div>
   </div>
 }
 
